bb72bdd1661e40e9b402727eee0e8674932489af
Niken Riri <nikenriri05@gmail.com>
2020-10-25 13:31:03 +0700

    revisi tugas 2

--- Tugas 2/Tugas2_123200091.cpp
+++ Tugas 2/Tugas2_123200091.cpp
@@ -18,19 +18,33 @@ int main()
     cout<<endl;
 
 
-    if(umura>20){
+    if(umura<=12){
+        b1=15000;
+    }else if (umura >12 && umura <=20){
+        b1=20000;
+    }else if (umura>20){
         b1=30000;
     }
-    if(umuri>20){
+    if(umuri<=12){
+        b2=15000;
+    }else if (umuri >12 && umuri <=20){
+        b2=20000;
+    }else if (umuri >20){
         b2=30000;
     }
     if(umura1<=12){
         b3=15000;
     }else if(umura1 >12 && umura1 <=20){
         b3=20000;
+    }else if (umura1 >20){
+        b3=30000;
     }
     if(umura2<=12){
         b4=15000;
+    }else if (umura2 >12 && umura2 <=20){
+        b4=20000;
+    }else if (umura2 >20){
+        b4=30000;
     }
     tagihan=30*(b1+b2+b3+b4);
     cout<<"Tagihan satu bulan adalah Rp."<<tagihan<<endl;
